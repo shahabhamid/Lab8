129bdf5e08bf4b3f2ef992b3b268d3e5bcf0982b
shahabhamid <59618242+shahabhamid@users.noreply.github.com>
2022-05-30 09:18:56 +0500

    Lab 8 Renamed & Task 3b: Interest Rate

--- src/lab/pkg8/FinancialCalc.java
+++ src/lab/pkg8/FinancialCalc.java
@@ -6,19 +6,32 @@ package lab.pkg8;
 public class FinancialCalc {
 
     public static void main(String[] args) {
-        double principal = 0.00;
+        double principal = 500;
         double interestRate = 0.05;
-        int numOfYears = 4;
-        double finalValue = 4000;
+        int numOfYears = 3;
+        double finalValue = 525.00;
 
-        principal = computerPrincipal(finalValue,interestRate,numOfYears);
+        interestRate = computeInterestRate(principal,finalValue,numOfYears);
 
-        System.out.println("The amount of money you should invest today "
-                + "(at an interest of 5%) to have a total amount of $1000 at the end of 3 years - "+principal);
+        System.out.println("The interest rate you need to turn an initial investment of $500.00 into $525.00 at the end of 3 years"
+                + " is "+interestRate);
     }
 
-     public static double computerPrincipal(double finalValue, double interestRate, double numOfYears) {
+    public static double computeFinalValue(double principal, double interestRate, int numOfYears) {
+        return principal * Math.pow((1 + interestRate), numOfYears);
+    }
+
+    public static double computeInterestRate(double principal, double finalValue, int numOfYears) {
+        return Math.pow((finalValue / principal), 1 / numOfYears)-1;
+
+    }
+
+    public static double computerPrincipal(double finalValue, double interestRate, double numOfYears) {
         return finalValue / Math.pow((1 + interestRate), numOfYears);
     }
     
+    public static double computerNumOfYears(double principal,double interestRate,double finalValue){
+        return Math.log((finalValue/principal)/Math.log(1+interestRate));
+        
+    } 
 }
