3d5be4785db29f1d7c895d798e19b458d7d8d5da
shahabhamid <59618242+shahabhamid@users.noreply.github.com>
2022-05-30 09:22:43 +0500

    Lab 8 Renamed & Task 3c: Number of Years

--- src/lab/pkg8/FinancialCalc.java
+++ src/lab/pkg8/FinancialCalc.java
@@ -6,15 +6,14 @@ package lab.pkg8;
 public class FinancialCalc {
 
     public static void main(String[] args) {
-        double principal = 500;
-        double interestRate = 0.05;
-        int numOfYears = 3;
-        double finalValue = 525.00;
+        double principal = 100;
+        double interestRate = 0.044;
+        double numOfYears = 0;
+        double finalValue = 150;
 
-        interestRate = computeInterestRate(principal,finalValue,numOfYears);
+        numOfYears = computeNumOfYears(principal,interestRate,finalValue);
 
-        System.out.println("The interest rate you need to turn an initial investment of $500.00 into $525.00 at the end of 3 years"
-                + " is "+interestRate);
+        System.out.println("The number of years you need to invest an initial sum of $100.00 at an interest rate of 4.4% to have a final value of $150.00 ="+numOfYears);
     }
 
     public static double computeFinalValue(double principal, double interestRate, int numOfYears) {
@@ -30,7 +29,7 @@ public class FinancialCalc {
         return finalValue / Math.pow((1 + interestRate), numOfYears);
     }
     
-    public static double computerNumOfYears(double principal,double interestRate,double finalValue){
+    public static double computeNumOfYears(double principal,double interestRate,double finalValue){
         return Math.log((finalValue/principal)/Math.log(1+interestRate));
         
     } 
